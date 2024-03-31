f4217b930140eaaf9adc174ea3932d587d0d06b4
Jacob Ormerod <jto888@users.noreply.github.com>
2024-03-31 18:29:58 -0400

    re-write examples()

--- package/CARMS/R/examples.carms.R
+++ package/CARMS/R/examples.carms.R
@@ -1,33 +1,35 @@
-examples.carms<- function(action="list", file=NULL)  {		
-	root<-system.file(package="CARMS")
-	# I expect backward directory slashes in linux (maybe MacOS too)
-	len<-nchar(root)
-	slash<- substr(root,len-5, len-5)
-	df<-data.frame(list.files(paste0(root,slash,"examples",slash)))
-	#df<-data.frame(list.files(paste0(root,"/examples/")))
-	names(df)<-"examples"
-	if(action=="run") {	
-	# need a series of tests and conditions for name and pattern
-		if(is(file,"numeric")) {
-		# need to assure file number is in range 1-48
-		if(!file<49 || !file>0) stop("file number out of range")
-			source(paste0(root,slash,"examples",slash,df[file,]))
-			#source(paste0(root,"/examples/",df[file,]))
-		}else{
-		if(!file %in% df$examples) stop("file name not found")
-			source(paste0(root,slash,"examples",slash, file))
-			#source(paste0(root,"/examples/",df[file,]))
-		}
-		ret=1
-	}else{	
-		if(action=="list") {
-			ret<-df
+examples.carms<- function(action="list", file=NULL)  {			
+	root<-system.file(package="CARMS")		
+	exdir<-paste0(root,"/examples/")		
+	df<-data.frame(list.files(exdir))		
+	names(df)<-"examples"		
+	if(action=="run" || action=="copy") {		
+		if(is(file,"numeric")) {	
+		# need to assure file number is in range 1-48	
+		if(!file<49 && !file>0) stop("file number out of range")	
+			file<-df[file,]
 		}else{	
-			ret=paste0(root,"/examples/")
-		}
-	}
-	
-ret	
-}
-
-examples<-examples.carms
+			if(!file %in% df$examples) stop("file name not found")
+		}	
+	}		
+	switch(action,		
+		"run" = {	
+			source(paste0(root,"/examples/",file))
+			ret<-1
+		},	
+		"copy" = {	
+			file.copy(paste0(exdir,file), paste0(getwd(),"/",file), overwrite=T)
+			ret<-TRUE
+		},	
+		"list"= {	
+			ret<-df
+		},	
+		"dir"= {	
+			ret<-exdir
+		},	
+	stop(paste0("No handler for ", action))		
+	)		
+ret			
+}			
+			
+examples<-examples.carms			
