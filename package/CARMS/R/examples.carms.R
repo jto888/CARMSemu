3ed14e6679d8e2bdc8baa54bd0c38a8f097b0842
Jacob Ormerod <jto888@users.noreply.github.com>
2024-03-31 18:47:01 -0400

    better examples function

--- package/CARMS/R/examples.carms.R
+++ package/CARMS/R/examples.carms.R
@@ -1,35 +1,38 @@
-examples.carms<- function(action="list", file=NULL)  {			
-	root<-system.file(package="CARMS")		
-	exdir<-paste0(root,"/examples/")		
-	df<-data.frame(list.files(exdir))		
-	names(df)<-"examples"		
-	if(action=="run" || action=="copy") {		
-		if(is(file,"numeric")) {	
-		# need to assure file number is in range 1-48	
-		if(!file<49 && !file>0) stop("file number out of range")	
-			file<-df[file,]
-		}else{	
-			if(!file %in% df$examples) stop("file name not found")
-		}	
-	}		
-	switch(action,		
-		"run" = {	
-			source(paste0(root,"/examples/",file))
-			ret<-1
-		},	
-		"copy" = {	
-			file.copy(paste0(exdir,file), paste0(getwd(),"/",file), overwrite=T)
-			ret<-TRUE
-		},	
-		"list"= {	
-			ret<-df
-		},	
-		"dir"= {	
-			ret<-exdir
-		},	
-	stop(paste0("No handler for ", action))		
-	)		
-ret			
-}			
-			
-examples<-examples.carms			
+				
+examples.carms<- function(action="list", file=NULL, package="CARMS")  {				
+	root<-system.file(package=package)			
+	exdir<-paste0(root,"/examples/")			
+	df<-data.frame(list.files(exdir))			
+	names(df)<-"examples"			
+	if(!is.null(file)) {			
+		if(is(file,"numeric")) {		
+		if(!file<(nrow(df)+1)) stop("file number out of range")		
+			file<-df[file,]	
+		}else{		
+			if(!file %in% df$examples) stop("file name not found")	
+		}		
+	}			
+	switch(action,			
+		"run" = {		
+			if(is.null(file)) stop("file argument is required")	
+			source(paste0(root,"/examples/",file))	
+			ret<-1	
+		},		
+		"copy" = {		
+			if(is.null(file)) stop("file argument is required")	
+			file.copy(paste0(exdir,file), paste0(getwd(),"/",file), overwrite=T)	
+			ret<-TRUE	
+		},		
+		"list"= {		
+			ret<-df	
+		},		
+		"dir"= {		
+			ret<-exdir	
+		},		
+	stop(paste0("No handler for ", action))			
+	)			
+ret				
+}				
+				
+examples<-examples.carms				
+		
