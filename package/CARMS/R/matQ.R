522c62f9316409389acacfbdea0e70c3fb75d736
Jacob Ormerod <jto888@users.noreply.github.com>
2024-05-10 12:17:19 -0400

    add matQ function

---
+++ package/CARMS/R/matQ.R
@@ -0,0 +1,15 @@
+#matQ.R
+
+# extracts the infinitessimal  generator matrix from a carms object simulation 
+
+matP<-function(x) {
+
+	# need to test that x is a carms object among perhaps other validations				
+	if(!is(x,"carms")){				
+		stop("x  argument is not of class  carms ")			
+	}				
+	if(is.null(x$simulation)) stop("carms object has no simulation")
+	
+	Q<-x$simulation$Q
+	Q
+}
